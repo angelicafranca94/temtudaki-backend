68382852185486857ffd39b39a85f2d869866a14
Angélica França <gefranca94@gmail.com>
2022-03-23 19:21:25 -0300

    API NESTJS Funcionando via Postaman

--- temtudaki-api/src/usuario/usuario.service.ts
+++ temtudaki-api/src/usuario/usuario.service.ts
@@ -1,5 +1,7 @@
 import { Injectable, Inject } from '@nestjs/common';
+import { ResultadoDto } from 'src/dto/resultado.dto';
 import { Repository } from 'typeorm';
+import { UsuarioCadastrarDto } from './dto/usuario.cadastrar.dto';
 import { Usuario } from './usuario.entity';
 
 @Injectable()
@@ -11,5 +13,27 @@ export class UsuarioService {
 
   async findAll(): Promise<Usuario[]> {
     return this.usuarioRepository.find();
+  }
+
+  async cadastrar(data: UsuarioCadastrarDto): Promise<ResultadoDto>{
+    
+    let usuario = new Usuario()
+    usuario.email = data.email
+    usuario.name = data.nome
+    usuario.password = data.senha
+
+    return this.usuarioRepository.save(usuario).then((result) =>{
+
+      return <ResultadoDto>{
+        status: true,
+        message: "Usuário Cadastrado com sucesso!"
+      }
+
+    }).catch((error) => {
+      return <ResultadoDto>{
+        status: false,
+        message: "Houve um erro ao cadastrar o Usuário."
+      }
+    })
   }
 }
