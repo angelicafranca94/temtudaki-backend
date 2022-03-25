4ae429a8205cbc77e7a1b1263bde92f569d96a02
Angélica França <gefranca94@gmail.com>
2022-03-25 16:56:10 -0300

    Implementado o refresh de token

--- temtudaki-api/src/auth/auth.service.ts
+++ temtudaki-api/src/auth/auth.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@nestjs/common';
 import { UsuarioService } from 'src/usuario/usuario.service';
 import * as bcrypt from 'bcrypt'
 import { JwtService } from '@nestjs/jwt';
+import { TokenService } from 'src/token/token.service';
 
 @Injectable()
 export class AuthService {
     constructor(private usuarioService: UsuarioService,
-      private jwtService: JwtService) {}
+      private jwtService: JwtService,
+      private tokenService: TokenService) {}
 
     async validarUsuario(email: string, senha: string): Promise<any> {
       const usuario = await this.usuarioService.findOne(email);
@@ -18,9 +20,11 @@ export class AuthService {
     }
 
     async login(user: any) {
-      const payload = { username: user.username, sub: user.userId };
+      const payload = { username: user.email, sub: user.id };
+      const token = this.jwtService.sign(payload)
+      this.tokenService.save(token, user.email)
       return {
-        access_token: this.jwtService.sign(payload),
+        access_token: token,
       };
     }
 }
---
+++ temtudaki-api/src/token/token.service.ts
@@ -0,0 +1,44 @@
+import { Injectable, Inject, HttpException, HttpStatus, forwardRef } from '@nestjs/common';
+import { AuthService } from 'src/auth/auth.service';
+import { UsuarioService } from 'src/usuario/usuario.service';
+import { Repository } from 'typeorm';
+import { Token } from './token.entity';
+
+@Injectable()
+export class TokenService {
+  constructor(
+    @Inject('TOKEN_REPOSITORY')
+    private tokenRepository: Repository<Token>,
+    private usuarioService: UsuarioService,
+    @Inject(forwardRef(() => AuthService))
+    private authService: AuthService
+  ) {}
+
+  async save(hash: string, username: string) {
+    let objToken = await this.tokenRepository.findOne({username: username})
+    if(objToken){
+
+      this.tokenRepository.update(objToken.id, {hash: hash})
+
+    }else{
+      this.tokenRepository.insert({
+        hash: hash,
+        username: username
+      })
+    }
+  }
+
+  async refreshToken(oldToken: string){
+    let objToken = await this.tokenRepository.findOne({hash: oldToken})
+    if(objToken){
+      let usuario = await this.usuarioService.findOne(objToken.username)
+      return this.authService.login(usuario)
+    }else{
+      // requisição inválida
+      return new HttpException({
+        errorMessage: 'Token inválido'
+      }, HttpStatus.UNAUTHORIZED)
+    }
+  }
+
+}
